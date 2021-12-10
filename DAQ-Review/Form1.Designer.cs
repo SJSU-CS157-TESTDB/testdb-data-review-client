1cbbea4cfc26080e362149ead9755cdd9ac6ca32
Greg <greglando@gmail.com>
2021-12-09 22:19:17 -0800

    Add project files.

---
+++ DAQ-Review/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DAQ_Review
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewTests = new System.Windows.Forms.ListView();
+            this.columnHeaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderProject = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderOrder = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanelData = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewTests
+            // 
+            this.listViewTests.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderName,
+            this.columnHeaderProject,
+            this.columnHeaderOrder,
+            this.columnHeaderStatus,
+            this.columnHeaderId});
+            this.listViewTests.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listViewTests.FullRowSelect = true;
+            this.listViewTests.HideSelection = false;
+            this.listViewTests.Location = new System.Drawing.Point(0, 0);
+            this.listViewTests.MultiSelect = false;
+            this.listViewTests.Name = "listViewTests";
+            this.listViewTests.Size = new System.Drawing.Size(472, 664);
+            this.listViewTests.TabIndex = 0;
+            this.listViewTests.UseCompatibleStateImageBehavior = false;
+            this.listViewTests.View = System.Windows.Forms.View.Details;
+            this.listViewTests.SelectedIndexChanged += new System.EventHandler(this.listViewTests_SelectedIndexChanged);
+            // 
+            // columnHeaderName
+            // 
+            this.columnHeaderName.Text = "Name";
+            this.columnHeaderName.Width = 120;
+            // 
+            // columnHeaderProject
+            // 
+            this.columnHeaderProject.Text = "Project";
+            this.columnHeaderProject.Width = 120;
+            // 
+            // columnHeaderOrder
+            // 
+            this.columnHeaderOrder.Text = "Order";
+            // 
+            // columnHeaderStatus
+            // 
+            this.columnHeaderStatus.Text = "Status";
+            this.columnHeaderStatus.Width = 80;
+            // 
+            // columnHeaderId
+            // 
+            this.columnHeaderId.Text = "Id";
+            // 
+            // tableLayoutPanelData
+            // 
+            this.tableLayoutPanelData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanelData.ColumnCount = 1;
+            this.tableLayoutPanelData.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelData.Location = new System.Drawing.Point(478, 0);
+            this.tableLayoutPanelData.Name = "tableLayoutPanelData";
+            this.tableLayoutPanelData.RowCount = 2;
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelData.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanelData.Size = new System.Drawing.Size(530, 631);
+            this.tableLayoutPanelData.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(527, 637);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "APPROVE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(749, 637);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(207, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "REJECT";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 664);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanelData);
+            this.Controls.Add(this.listViewTests);
+            this.Name = "FormMain";
+            this.Text = "DAQ Reviewer";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewTests;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderProject;
+        private System.Windows.Forms.ColumnHeader columnHeaderOrder;
+        private System.Windows.Forms.ColumnHeader columnHeaderStatus;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelData;
+        private System.Windows.Forms.ColumnHeader columnHeaderId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DAQ-Review/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace DAQ_Review
+{
+    public partial class FormMain : Form
+    {
+        public static string connString = "Host=localhost;Username=postgres;Password=.;Database=testdb";
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void FormMain_Load(object sender, EventArgs e)
+        {
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                await connection.OpenAsync();
+
+                var cmd = new NpgsqlCommand("SELECT * FROM public.\"Unreviewed Test Data\"", connection);
+
+                var reader = await cmd.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    listViewTests.Items.Add(new ListViewItem(new string[] { reader.GetString(3), reader.GetString(1), reader.GetInt64(2).ToString(), reader.GetString(4), reader.GetInt64(0).ToString() }));
+                }
+            }
+               
+        }
+
+        private async void listViewTests_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tableLayoutPanelData.Controls.Clear();
+            var it = listViewTests.SelectedItems[0];
+            var task_id = Int64.Parse(it.SubItems[4].Text);
+
+            using (var connection = new NpgsqlConnection(connString))
+            {
+                await connection.OpenAsync();
+                var cmd = new NpgsqlCommand("SELECT * FROM public.test_data RIGHT JOIN tests ON tests.test_id = test_data.test WHERE tests.task_id = @id", connection);
+                cmd.Parameters.AddWithValue("id", task_id);
+
+                var reader = await cmd.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    var data_type = reader.GetString(2);
+
+                    if (data_type == "dbm_scalar")
+                    {
+                        var lbl = new Label();
+                        lbl.Text = reader.GetString(3) + " dBm";
+                        tableLayoutPanelData.Controls.Add(lbl);
+                    }
+                    else if (data_type == "spectrum_plot_url")
+                    {
+                        var img = new PictureBox();
+                        img.SizeMode = PictureBoxSizeMode.AutoSize;
+                        img.ImageLocation = reader.GetString(3);
+                        tableLayoutPanelData.Controls.Add(img);
+                    }
+                }
+            }
+
+        }
+    }
+}
